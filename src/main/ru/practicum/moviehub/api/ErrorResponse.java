25d37107492060b4ecc8b38d11c829851ba04445
Anna <mussina1anna@yandex.ru>
2026-02-22 18:54:04 +0300

    Fix Checkstyle errors in ErrorResponse

--- src/main/ru/practicum/moviehub/api/ErrorResponse.java
+++ src/main/ru/practicum/moviehub/api/ErrorResponse.java
@@ -7,17 +7,29 @@ public class ErrorResponse {
     private String error;
     private List<String> details;
 
-    public ErrorResponse() {}
+    public ErrorResponse() {
+    }
 
     public ErrorResponse(String error, List<String> details) {
         this.error = error;
         this.details = details;
     }
 
-    public String getError() { return error; }
-    public void setError(String error) { this.error = error; }
-    public List<String> getDetails() { return details; }
-    public void setDetails(List<String> details) { this.details = details; }
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public List<String> getDetails() {
+        return details;
+    }
+
+    public void setDetails(List<String> details) {
+        this.details = details;
+    }
 
     @Override
     public boolean equals(Object o) {
