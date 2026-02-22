08c57b19c782be3b6f8eb1fd817cacee7bca4ba5
Anna <mussina1anna@yandex.ru>
2026-02-22 18:56:20 +0300

    Fix Checkstyle formatting in Movie

--- src/main/ru/practicum/moviehub/model/Movie.java
+++ src/main/ru/practicum/moviehub/model/Movie.java
@@ -7,19 +7,37 @@ public class Movie {
     private String title;
     private int year;
 
-    public Movie() {}
+    public Movie() {
+    }
 
     public Movie(String title, int year) {
         this.title = title;
         this.year = year;
     }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-    public int getYear() { return year; }
-    public void setYear(int year) { this.year = year; }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 
     @Override
     public boolean equals(Object o) {
